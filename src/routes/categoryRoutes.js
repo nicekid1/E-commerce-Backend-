1b364c77e10b0a9e8328efb3ef6a305f07f0ca25
nicekid1 <86746988+nicekid1@users.noreply.github.com>
2024-12-10 23:32:15 +0330

    feat: add functionality to fetch all categories in category routes

--- src/routes/categoryRoutes.js
+++ src/routes/categoryRoutes.js
@@ -1,17 +1,27 @@
 const express = require("express");
 const Category = require("../models/Category");
+const auth = require("../middlewares/auth");
 const router = express.Router();
 
 //Add category
-router.post('/', async (req, res) => {
+router.post('/',auth, async (req, res) => {
   const { name, description } = req.body;
   try {
     const category = new Category({ name, description });
-    category.save();
+    await category.save();
     res.status(201).json(category);
   } catch (err) {
     res.status(400).json({ message: "Error adding category", error: err });
   }
 });
 
+//Get all categories
+router.get('/',auth, async(req, res)=>{
+  try {
+    const categories = await Category.find()
+    res.status(200).json(categories);
+  } catch (err) {
+    res.status(500).json({ message: 'Error retrieving categories', error: err });
+  }
+})
 module.exports = router;
