0cc6848acd625933dea83ffd16eddce1e6856de0
nicekid1 <86746988+nicekid1@users.noreply.github.com>
2024-12-10 22:37:44 +0330

    feat: add orders route with functionality to place new orders

--- app.js
+++ app.js
@@ -1,11 +1,10 @@
-const express = require('express');
-const dotenv = require('dotenv');
-const mongoose = require('mongoose');
-const productRoutes = require('./src/routes/productRoutes');
-const userRoutes = require('./src/routes/userRoutes');
-const cartRoutes = require('./src/routes/cartRoutes');
-
-
+const express = require("express");
+const dotenv = require("dotenv");
+const mongoose = require("mongoose");
+const productRoutes = require("./src/routes/productRoutes");
+const userRoutes = require("./src/routes/userRoutes");
+const cartRoutes = require("./src/routes/cartRoutes");
+const orderRoutes = require("./src/routes/orderRoutes");
 
 // Load environment variables
 dotenv.config();
@@ -16,30 +15,31 @@ const app = express();
 app.use(express.json());
 
 // Connect to MongoDB
-mongoose.connect(process.env.MONGO_URI)
-.then(() => {
-    console.log('Connected to MongoDB');
-})
-.catch((error) => {
-    console.error('Error connecting to MongoDB:', error.message);
-});
-mongoose.connection.once('open', () => {
-    console.log('Database Name:', mongoose.connection.db.databaseName);
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => {
+    console.log("Connected to MongoDB");
+  })
+  .catch((error) => {
+    console.error("Error connecting to MongoDB:", error.message);
+  });
+mongoose.connection.once("open", () => {
+  console.log("Database Name:", mongoose.connection.db.databaseName);
 });
 
 // Simple route to test the server
-app.get('/', (req, res) => {
-    res.send('Welcome to E-commerce APIs');
+app.get("/", (req, res) => {
+  res.send("Welcome to E-commerce APIs");
 });
 
 //Routes
-app.use('/api/products', productRoutes);
-app.use('/api/auth', userRoutes);
-app.use('/api/cart', cartRoutes)
-
+app.use("/api/products", productRoutes);
+app.use("/api/auth", userRoutes);
+app.use("/api/cart", cartRoutes);
+app.use("/api/order", orderRoutes);
 
 // Start the server
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-    console.log(`Server is running on http://localhost:${PORT}`);
+  console.log(`Server is running on http://localhost:${PORT}`);
 });
---
+++ src/routes/orderRoutes.js
@@ -0,0 +1,46 @@
+const express = require("express");
+const Order = require("../models/Order");
+const Cart = require("../models/Cart");
+const Product = require("../models/Product");
+const router = express.Router();
+
+// Place a new order
+router.post("/:userId", async (req, res) => {
+  const { userId } = req.params;
+  try {
+    const cart = await Cart.findOne({ user:userId }).populate("items.product");
+    if (!cart || cart.items.length === 0) {
+      return res.status(400).json({ message: "Cart is empty" });
+    }
+
+    const items = cart.items.map((item) => ({
+      product: item.product,
+      quantity: item.quantity,
+    }));
+
+    const totalAmount = items.reduce(
+      (total, item) => total + item.quantity * item.product.price,
+      0
+    );
+
+    const newOrder = new Order({
+      user: userId,
+      items,
+      totalAmount,
+      status: "pending",
+    });
+
+    await newOrder.save();
+    await Cart.findOneAndDelete({ userId });
+
+    res
+      .status(201)
+      .json({ message: "Order placed successfully", order: newOrder });
+  } catch (err) {
+    res
+      .status(500)
+      .json({ message: "Error placing order", error: err.message });
+  }
+});
+
+module.exports = router;
