72c125c6cbda58f439f886a6cacb295c883059c0
nicekid1 <86746988+nicekid1@users.noreply.github.com>
2024-12-10 23:58:15 +0330

    fix: resolve syntax error in auth middleware

--- src/middlewares/auth.js
+++ src/middlewares/auth.js
@@ -1,16 +1,17 @@
 const jwt = require("jsonwebtoken");
 
 const auth = (req, res, next) => {
-  const token = req.header("Authorization");
+  const token = req.header("Authorization")?.split(" ")[1];
   if (!token)
     return res
       .status(401)
       .json({ message: "Access denied. No token provided." });
   try {
-    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    const decoded = jwt.verify(token, process.env.SECRET_KEY);
     req.user = decoded;
     next();
   } catch (err) {
+    console.log(err)
     res.status(400).json({ message: "Invalid token." });
   }
 };
