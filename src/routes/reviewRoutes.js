7b318e2a59e6544ac9266a4803200419c0ec6ba6
nicekid1 <86746988+nicekid1@users.noreply.github.com>
2024-12-11 18:21:54 +0330

    feat: add functionality to fetch reviews for a specific product

--- src/routes/reviewRoutes.js
+++ src/routes/reviewRoutes.js
@@ -1,20 +1,21 @@
-
 const express = require("express");
-const mongoose = require("mongoose"); 
+const mongoose = require("mongoose");
 const Review = require("../models/Review");
 const auth = require("../middlewares/auth");
-
 const router = express.Router();
 
+//Add a comment to the product
 router.post("/:productId", auth, async (req, res) => {
   const { comment, rating } = req.body;
   const { productId } = req.params;
   const userId = req.user.userId;
-  console.log(userId)
+  console.log(userId);
 
   try {
     if (!rating || rating < 1 || rating > 5) {
-      return res.status(400).json({ message: "Rating must be between 1 and 5" });
+      return res
+        .status(400)
+        .json({ message: "Rating must be between 1 and 5" });
     }
     if (!comment || comment.trim() === "") {
       return res.status(400).json({ message: "Comment cannot be empty" });
@@ -33,7 +34,23 @@ router.post("/:productId", auth, async (req, res) => {
 
     res.status(201).json({ message: "Review added successfully", review });
   } catch (err) {
-    res.status(500).json({ message: "Error adding comment", error: err.message });
+    res
+      .status(500)
+      .json({ message: "Error adding comment", error: err.message });
+  }
+});
+
+//Get specific product reviews
+router.get("/:productId", auth, async (req, res) => {
+  const { productId } = req.params;
+  try {
+    const comments = await Review.find({ product:productId }).populate(
+      "user",
+      "username"
+    );
+    res.status(200).json(comments);
+  } catch (err) {
+    res.status(500).json({ message: "Error retrieving comments", error: err });
   }
 });
 
