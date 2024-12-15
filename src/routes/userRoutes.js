15ed11c5e1e607ecb403b540fee02d37c5e6e372
nicekid1 <86746988+nicekid1@users.noreply.github.com>
2024-12-15 22:08:47 +0330

    feat: add functionality for users to remove a favorite item in user routes

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -4,7 +4,7 @@ const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 const router = express.Router();
 const Product = require("./../models/Product");
-const auth = require('./../middlewares/auth')
+const auth = require("./../middlewares/auth");
 
 //register
 router.post("/register", async (req, res) => {
@@ -45,7 +45,7 @@ router.post("/login", async (req, res) => {
 });
 
 //Add product to favorites
-router.post("/favorites/:productId",auth ,async (req, res) => {
+router.post("/favorites/:productId", auth, async (req, res) => {
   try {
     const product = await Product.findById(req.params.productId);
     if (!product) return res.status(404).json({ message: "Product not found" });
@@ -56,14 +56,30 @@ router.post("/favorites/:productId",auth ,async (req, res) => {
         .json({ message: "Product has already been added." });
     user.favoriteProducts.push(product._id);
     await user.save();
-    res
-      .status(200)
-      .json({
-        message: "Product added to favorites",
-        favoriteProducts: user.favoriteProducts,
-      });
+    res.status(200).json({
+      message: "Product added to favorites",
+      favoriteProducts: user.favoriteProducts,
+    });
   } catch (err) {
-    res.status(500).json({ message:'Server error' , error: err.message });
+    res.status(500).json({ message: "Server error", error: err.message });
   }
 });
+
+//Remove product from favorites
+router.delete("/favorites/:productId", auth, async (req, res) => {
+  try {
+    const user = await User.findById(req.user.userId);
+    user.favoriteProducts = user.favoriteProducts.filter(
+      (id) => id.toString() !== req.params.productId
+    );
+    await user.save();
+    res.status(200).json({
+      message: "Product removed from favorites",
+      favoriteProducts: user.favoriteProducts,
+    });
+  } catch (err) {
+    res.status(500).json({ message: "Server error", error: err.message });
+  }
+});
+
 module.exports = router;
